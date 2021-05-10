ff32d3ba69a6e5a94130d11bbcac3dc6709bbb3e
Ben Lovell <bjl20@ic.ac.uk>
2021-05-10 14:30:00 +0100

    lecture

--- Week 3 - More functional/web-app/game_2048.js
+++ Week 3 - More functional/web-app/game_2048.js
@@ -1,11 +1,23 @@
+//const board = [
+ //   [1, 1, 0, 0],
+ //   [1, 0, 2, 0],
+ //   [0, 2, 0, 2],
+ //   [1, 1, 2, 0],
+
 const game_2048 = Object.create(null);
 
 // The identity function returns whatever it is passed. i.e does nothing.
 const identity = (x) => x;
 
-game_2048.left = identity; // this is a placeholder for a correct implementation
-game_2048.right = identity;
+game_2048.left = identity;// this is a placeholder for a correct implementation
+
+const row_flip = (row) => row.slice().reverse();
+const h_flip = (board) => board.map(row_flip);
+
+game_2048.right = (board) => h_flip(game_2048.left(h_flip(board)));
+
 game_2048.up = identity;
+
 game_2048.down = identity;
 
 export default Object.freeze(game_2048);
